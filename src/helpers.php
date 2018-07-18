ca0cb416356c2226aecfeb9c35793f801be6a055
baijunyao <baijunyao@baijunyao.com>
2018-07-18 16:05:35 +0800

    增加用于简化if判断的参数

--- src/helpers.php
+++ src/helpers.php
@@ -4,12 +4,22 @@ if (!function_exists('flash_success')){
     /**
      * 添加成功提示
      *
-     * @param string $message
+     * @param string $message  提示内容
+     * @param bool   $flash    当为 true 的时候会触发提示 此参数是为了方便在外部调用的时减少判断
+     *                         例如原本的if判断场景:
+     *                         if($needFlash) {
+     *                              flash_success('成功')
+     *                         }
+     *                         用此参数可以简化成
+     *                         flash_success('成功', $needFlash)
+     *
      */
-    function flash_success($message = '成功')
+    function flash_success($message = '成功', $flash = true)
     {
-        session()->flash('alert-message', $message);
-        session()->flash('alert-type', 'success');
+        if ($flash) {
+            session()->flash('alert-message', $message);
+            session()->flash('alert-type', 'success');
+        }
     }
 }
 
@@ -17,12 +27,21 @@ if (!function_exists('flash_error')){
     /**
      * 添加失败提示
      *
-     * @param string $message
+     * @param string $message  提示内容
+     * @param bool   $flash    当为 true 的时候会触发提示 此参数是为了方便在外部调用的时减少判断
+     *                         例如原本的if判断场景:
+     *                         if($needFlash) {
+     *                              flash_error('失败')
+     *                         }
+     *                         用此参数可以简化成
+     *                         flash_error('失败', $needFlash)
      */
-    function flash_error($message = '失败')
+    function flash_error($message = '失败', $flash = true)
     {
-        session()->flash('alert-message', $message);
-        session()->flash('alert-type', 'error');
+        if ($flash) {
+            session()->flash('alert-message', $message);
+            session()->flash('alert-type', 'error');
+        }
     }
 }
 
